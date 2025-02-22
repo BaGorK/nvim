b1ce7a7633679b650480d99a30c82437033c68ea
Edmealem kassahun <edmealemkassahun@gmail.com>
2025-02-22 22:23:23 +0300

    master:update: options.lua to wrap linebreak and format long texts without being cut off the screen

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,4 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
--- vim.opt.winbar = "%=%m %f"
+
+vim.opt.wrap = true -- Enable soft wrapping
+vim.opt.linebreak = true -- Prevent words from splitting in the middle
+vim.opt.breakindent = true -- Indent wrapped lines
+vim.opt.showbreak = "↳ " -- Show an indicator for wrapped lines
