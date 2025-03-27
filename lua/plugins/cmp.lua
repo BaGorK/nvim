caf0ace3629c421fe3d61500515c4c43359595b8
Edmealem kassahun <edmealemkassahun@gmail.com>
2025-03-27 09:31:36 +0300

    master:vim-dadbod and auto pair

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,4 +1,35 @@
 return {
+  { "echasnovski/mini.pairs", enabled = true },
+  {
+    "hrsh7th/nvim-cmp",
+    enabled = true,
+    event = "InsertEnter",
+    dependencies = {
+      "windwp/nvim-autopairs",
+      "windwp/nvim-ts-autotag", -- Add for automatic tag closing
+    },
+    config = function()
+      local cmp = require("cmp")
+      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+
+      cmp.event:on("confirm_done", function(...)
+        cmp_autopairs.on_confirm_done()(...)
+      end)
+
+      cmp.setup({
+        mapping = {
+          ["<Tab>"] = function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            else
+              fallback()
+            end
+          end,
+        },
+      })
+    end,
+  },
+
   {
     "rafamadriz/friendly-snippets",
     enabled = false, -- Disable friendly-snippets
