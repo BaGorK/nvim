fb1d43fa88968ffd23687445b36464973dbd06fb
Edmealem kassahun <edmealemkassahun@gmail.com>
2025-04-24 04:49:06 +0300

    master:code-snap and prisma

---
+++ lua/plugins/code-snap.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "mistricky/codesnap.nvim",
+    build = "make build_generator",
+    event = "VeryLazy",
+
+    keys = {
+      { "<leader>cs", "<Esc><cmd>CodeSnap<cr>", mode = "x", desc = "Save selected code snapshot into clipboard" },
+      { "<leader>cS", "<Esc><cmd>CodeSnapSave<cr>", mode = "x", desc = "Save selected code snapshot in ~/Pictures/" },
+    },
+    lazy = true,
+    opts = {
+      mac_window_bar = false,
+      save_path = "~/Pictures/",
+      has_breadcrumbs = true,
+      show_workspace = true,
+      bg_theme = "sea",
+      watermark = "",
+      code_font_family = "FiraCode Nerd Font",
+      has_line_number = true,
+    },
+  },
+}
---
+++ lua/plugins/tree-sitter.lua
@@ -0,0 +1,36 @@
+if true then
+  return {}
+end
+
+return { -- Highlight, edit, and navigate code
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  main = "nvim-treesitter.configs", -- Sets main module to use for opts
+  opts = {
+    ensure_installed = {
+      "go",
+      "javascript",
+      "typescript",
+      "vue",
+      "bash",
+      "c",
+      "diff",
+      "html",
+      "lua",
+      "luadoc",
+      "markdown",
+      "markdown_inline",
+      "query",
+      "vim",
+      "vimdoc",
+    },
+    -- Autoinstall languages that are not installed
+    auto_install = true,
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = { "ruby" },
+    },
+    indent = { enable = true, disable = { "ruby" } },
+    autotag = { enable = true },
+  },
+}
